b7e7b929d60d07ed6dbb8e54746dfc04a325e53d
HH1080-bit <71850199+HH1080-bit@users.noreply.github.com>
2023-06-05 22:14:18 +0300

    Add files via upload

---
+++ src/Components/Volunteers/Volunteers.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Volunteers.module.css'
+
+const Volunteers = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Volunteers;
