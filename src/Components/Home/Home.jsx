ab5068b25e118d5ddeede8be5d607a8e7792358b
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2023-06-17 11:29:11 +0300

    pulling

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -16,87 +16,78 @@ const Home = () => {
   };
   return (
     <>
-      <div
-        className={`${styles.landing} d-flex justify-content-center align-items-start`}
-      >
-        <div className="landing-content">
-          <div className=" text-center">
-            <h2
-              style={{
-                letterSpacing: 4,
-                fontSize: "3.5rem",
-                fontFamily: "Montserrat Alternates",
-                fontWeight: "800",
-                marginTop: "3rem",
-                marginBottom: 0,
-                color: "#ff4951",
-              }}
+      <View style={styles.overlay} onTouchStart={toggleSelection}></View>
+      <View style={`${styles.selection} w-75`}>
+        <View style={styles.container}>
+          <Text style={styles.heading}>Choose Your Plan</Text>
+          <View style={styles.row}>
+            <TouchableOpacity
+              style={`${styles.option} rounded`}
+              onPress={handleOptionOne}
+              ref={optionOne}
             >
-              BLOOD <br /> DONATION
-            </h2>
-            <p
-              style={{
-                letterSpacing: 4,
-                fontSize: "2.5rem",
-                fontFamily: "Covered By Your Grace",
-                fontWeight: "bold",
-                marginBottom: 5,
-              }}
+              <Text style={styles.optionText}>Organization</Text>
+              <BsHospital size={64} color="#ee394a" />
+              <Text style={styles.optionDescription}>
+                This Plan Include Hospitals and Blood Banks
+              </Text>
+              {isOptionOneSelected && (
+                <Text style={styles.selectedText}>
+                  * You are Following the Organizational Plan
+                </Text>
+              )}
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={`${styles.option} rounded`}
+              onPress={handleOptionTwo}
+              ref={optionTwo}
             >
-              Saves Lifes,
-            </p>
-            <div className="mb-3">
-              <span
-                style={{
-                  letterSpacing: 4,
-                  backgroundColor: "#fbd6e7",
-                  fontSize: "1rem",
-                  fontFamily: "Montserrat Alternates",
-                  fontWeight: "bold",
-                  padding: 8,
-                  borderRadius: 20,
-                }}
+              <Text style={styles.optionText}>User</Text>
+              <AiOutlineUser size={64} color="#ee394a" />
+              <Text style={styles.optionDescription}>
+                This Plan Include Donors and Recipients
+              </Text>
+              {isOptionTwoSelected && (
+                <Text style={styles.selectedText}>
+                  * You are Following the Users Plan
+                </Text>
+              )}
+            </TouchableOpacity>
+          </View>
+
+          {isOptionOneSelected && (
+            <View style={styles.buttons}>
+              <TouchableOpacity
+                style={styles.cancelButton}
+                onPress={toggleSelection}
               >
-                Together we are stronger
-              </span>
-            </div>
-
-            <p
-              className="text-black-50 mb-3"
-              style={{
-                fontSize: "1rem",
-                fontFamily: "Montserrat Alternates",
-                fontWeight: "bold",
-                maxWidth: "500px",
-              }}
-            >
-              Find blood donors near your location and make a blood request in
-              less than 5 minutes.
-            </p>
-            <button className="btn btn-danger fs-5" onClick={handleSelection}>
-              Donate Now
-            </button>
-          </div>
-
-          {isSelectionActive && (
-            <Selection onSelection={setIsSelectionActive} />
+                <Text style={styles.buttonText}>Cancel</Text>
+              </TouchableOpacity>
+              <Link to="signup-org">
+                <TouchableOpacity style={styles.signupButton}>
+                  <Text style={styles.buttonText}>Sign Up</Text>
+                </TouchableOpacity>
+              </Link>
+            </View>
           )}
-        </div>
-      </div>
-      <div
-        className="row container mx-auto text-center mt-5 p-5"
-        data-aos="fade-up"
-      >
-        <InfoHome></InfoHome>
-      </div>
-      <div className="text-center mt-5">
-        <h2 className="main-color">Join The Cause</h2>
-        <p className="text-muted">
-          Join our cause and help us save more lives. Everyone should have the
-          right to get a blood transfusion.
-        </p>
-      </div>
-      <CauseHome></CauseHome>
+
+          {isOptionTwoSelected && (
+            <View style={styles.buttons}>
+              <TouchableOpacity
+                style={styles.cancelButton}
+                onPress={toggleSelection}
+              >
+                <Text style={styles.buttonText}>Cancel</Text>
+              </TouchableOpacity>
+              <Link to="signup-user">
+                <TouchableOpacity style={styles.signupButton}>
+                  <Text style={styles.buttonText}>Sign Up</Text>
+                </TouchableOpacity>
+              </Link>
+            </View>
+          )}
+        </View>
+      </View>
     </>
   );
 };
