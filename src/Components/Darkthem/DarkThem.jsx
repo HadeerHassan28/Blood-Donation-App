4e6364b41ac495778e5bc723d1e34210765f2219
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2023-06-20 12:04:45 +0300

    pushing

--- src/Components/Darkthem/DarkThem.jsx
+++ src/Components/Darkthem/DarkThem.jsx
@@ -1,20 +1,9 @@
-<<<<<<< HEAD:src/Components/Darkthem/DarkThem.js
 // import { useState } from "react";
 // import { ThemeProvider, createTheme } from "@mui/material/styles";
 // import CssBaseline from "@mui/material/CssBaseline";
 
 // function ToggleColorMode() {
 //   const [theme, setTheme] = useState(false);
-=======
-import { useState } from "react";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import { useTranslation } from 'react-i18next';
-
-function ToggleColorMode() {
-  const [theme, setTheme] = useState(false);
-  const {t} = useTranslation();
->>>>>>> b02ec2b78ea6ad96cffb58316e8eca34b0da5ab7:src/Components/Darkthem/DarkThem.jsx
 
 //   const toggleTheme = () => {
 //     setTheme((prevTheme) => !prevTheme);
@@ -26,21 +15,11 @@ function ToggleColorMode() {
 //     },
 //   });
 
-<<<<<<< HEAD:src/Components/Darkthem/DarkThem.js
 //   return;
 //      <ThemeProvider theme={themeConfig}>
 //      <CssBaseline />
 //       <button onClick={toggleTheme}>Toggle Theme</button>
 //    </ThemeProvider>
 // }
-=======
-  return (
-    <ThemeProvider theme={themeConfig}>
-      <CssBaseline />
-      <button onClick={toggleTheme}>{t("Dark Mode")}</button>
-    </ThemeProvider>
-  );
-}
->>>>>>> b02ec2b78ea6ad96cffb58316e8eca34b0da5ab7:src/Components/Darkthem/DarkThem.jsx
 
 // export default ToggleColorMode;
